b17c42e2165d4bbb0942504c6e7afcc8bdce9645
Mister-deal <max-94400@hotmail.fr>
2025-07-02 15:56:16 +0200

    ajout methodes et initialisation classe

--- duel/Classes/Guerrier.cs
+++ duel/Classes/Guerrier.cs
@@ -1,6 +1,87 @@
 ﻿namespace duel.Classes;
 
-public abstract class Guerrier
+public class Guerrier
 {
+    // Champs privés
+    private string _nom;
+    private int _pointsDeVie;
+    private int _nbDesAttaque;
+
+    // Constructeur
+    public Guerrier(string nom, int pointsDeVie, int nbDesAttaque)
+    {
+        _nom = nom;
+        _pointsDeVie = pointsDeVie;
+        _nbDesAttaque = nbDesAttaque;
+    }
+
+    // Propriétés publiques
+    public string Nom
+    {
+        get => _nom;
+        set => _nom = value ?? throw new ArgumentNullException(nameof(value));
+    }
+
+    public int PointsDeVie
+    {
+        get => _pointsDeVie;
+        set => _pointsDeVie = value;
+    }
+
+    public int NbDesAttaque
+    {
+        get => _nbDesAttaque;
+        set => _nbDesAttaque = value;
+    }
+
+
+    public string GetNom()
+    {
+        return $"{Nom} est le guerrier que vous avez selectionné";
+    }
+
+    public int GetPointsDeVie()
+    {
+        return PointsDeVie;
+    }
+
+    public void SetPointDeVie(int pointsDeVie)
+    {
+        PointsDeVie = pointsDeVie;
+        Console.WriteLine($"les points de vie attribués sont: {pointsDeVie}");
+    }
+
+    public int GetNbDesAttaques()
+    {
+        return NbDesAttaque;
+    }
+
+    public void AfficherInfos()
+    {
+        Console.WriteLine($"{Nom}: [PV= {PointsDeVie}, Nbr= {NbDesAttaque}]");
+    }
+
+    public int Attaquer()
+    {
+        Random random  = new Random();
+        int totalDamage = 0;
+        for (int i = 0; i < NbDesAttaque; i++)
+        {
+            totalDamage += random.Next(1, 7);
+        }
+        return totalDamage;
+    }
+
+    public void SubirDegats(int degats)
+    {
+        Console.Write(PointsDeVie);
+        PointsDeVie -= degats;
+        Console.WriteLine($" - {degats} = {PointsDeVie} de point de vie actuel");
+        if (PointsDeVie <= 0)
+        {
+            Console.WriteLine($"{Nom} a été vaincu !");
+        }
+    }
+    
     
 }
--- duel/Program.cs
+++ duel/Program.cs
@@ -1,2 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using duel.Classes;
+
+Guerrier lancelot = new Guerrier("Lancelot", 35, 3);
+Guerrier galahad = new Guerrier("galahad", 30, 4);
+
+
+lancelot.AfficherInfos();
+galahad.AfficherInfos();
+
+galahad.SubirDegats(lancelot.Attaquer());
+galahad.AfficherInfos();
+
+lancelot.SubirDegats(galahad.Attaquer());
+lancelot.AfficherInfos();
+
+galahad.SubirDegats(lancelot.Attaquer());
+galahad.AfficherInfos();
+
+lancelot.SubirDegats(galahad.Attaquer());
+lancelot.AfficherInfos();
+
+galahad.SubirDegats(lancelot.Attaquer());
+galahad.AfficherInfos();
+
+lancelot.SubirDegats(galahad.Attaquer());
+lancelot.AfficherInfos();
+
+
+
